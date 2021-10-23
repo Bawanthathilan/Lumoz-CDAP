cd70a0104d62780e4b7644ab348e19612e5f7f7b
Bawanthathilan <bawantharathnayaka@gmail.com>
2021-10-24 03:40:01 +0530

    added back to top btn

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,3 +14,24 @@ function closeMenu() {
   hamburger.classList.remove('active')
   navMenu.classList.remove('active')
 }
+
+var mybutton = document.getElementById('backBtn')
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+  scrollFunction()
+}
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = 'block'
+  } else {
+    mybutton.style.display = 'none'
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0
+  document.documentElement.scrollTop = 0
+}
